dd057583e76f38f2f1c5d9969a7be17c2d9edb9f
rucharajmane <rucharajmane18@gmail.com>
2024-11-08 13:08:37 +0530

    Quick Sort

--- DAA/A5.cpp
+++ DAA/A5.cpp
@@ -11,24 +11,21 @@ int partition(vector<int> &arr, int low, int high)
 
     while (i < j)
     {
-        // Move `i` to the right until we find an element greater than the pivot
         do
         {
             i++;
         } while (arr[i] <= pivot && i < high);
 
-        // Move `j` to the left until we find an element less than or equal to the pivot
         do
         {
             j--;
         } while (arr[j] > pivot);
 
         if (i < j)
-            swap(arr[i], arr[j]); // Swap elements to ensure they are on the correct side of the pivot
+            swap(arr[i], arr[j]);
     }
-
-    swap(arr[low], arr[j]); // Place the pivot in its correct position
-    return j;               // Return the partition point
+    swap(arr[low], arr[j]);
+    return j;
 }
 
 void QuickSort(vector<int> &arr, int low, int high)
@@ -45,12 +42,11 @@ void QuickSortR(vector<int> &arr, int low, int high)
 {
     if (low < high)
     {
-        // Choose a random pivot and swap with the first element
         int random = low + rand() % (high - low);
         swap(arr[random], arr[low]);
-        int p = partition(arr, low, high); // Get the partition index
-        QuickSortR(arr, low, p);           // Recursively sort elements before partition
-        QuickSortR(arr, p + 1, high);      // Recursively sort elements after partition
+        int p = partition(arr, low, high);
+        QuickSortR(arr, low, p);
+        QuickSortR(arr, p + 1, high);
     }
 }
 
@@ -66,7 +62,7 @@ int main()
         cout << i << " ";
     cout << endl;
 
-    x = {10, 12, 15, 20, 1, 4, 17, 16, 11}; 
+    x = {10, 12, 15, 20, 1, 4, 17, 16, 11};
     QuickSortR(x, low, high);
     for (auto i : x)
         cout << i << " ";
